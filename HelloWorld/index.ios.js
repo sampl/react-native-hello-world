60f63525ec435198b2879790752c10ccb5dd75ba
Sam Pierce Lolla <sampiercelolla@gmail.com>
2016-10-27 11:53:44 -0400

    make an Animal component

--- HelloWorld/index.ios.js
+++ HelloWorld/index.ios.js
@@ -3,24 +3,38 @@ import { AppRegistry, Text, Image, View } from 'react-native'
 
 class HelloWorld extends Component {
   render() {
-    let sloth = {
+
+    let slothPic = {
       uri: 'https://i.imgur.com/UH59bBo.jpg'
     }
-    let bear = {
+    let bearPic = {
       uri: 'https://s-media-cache-ak0.pinimg.com/564x/3c/4b/90/3c4b9031ed852ea25f587e34c34a9569.jpg'
     }
-    let slothText = 'hello darling'
-    let bearText = 'here we go'
 
     return (
       <View>
-        <Image source={sloth} style={{width: 320, height: 200}}/>
-        <Text>{slothText}</Text>
-        <Image source={bear} style={{width: 320, height: 200}}/>
-        <Text>{bearText}</Text>
+        <Animal caption="hello darlinggg" pic={slothPic} />
+        <Animal caption="here we gooo" pic={bearPic} />
       </View>
     )
   }
 }
 
+class Animal extends Component {
+  render() {
+    let viewStyle = {
+      alignItems: 'center',
+    }
+    let picStyle = {
+      width: 320,
+      height: 200,
+    }
+    return(
+      <View style={viewStyle}>
+        <Image source={this.props.pic} style={picStyle}/>
+        <Text>{this.props.caption}</Text>
+      </View>
+    )
+  }
+}
 AppRegistry.registerComponent('HelloWorld', () => HelloWorld)
